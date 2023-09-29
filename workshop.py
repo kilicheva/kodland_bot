473661611193e21a8c312c07f7518008b42bb3e2
kilicheva <260495aa@gmail.com>
2023-09-29 22:51:58 +0300

    Add lesson M2L1 part 6 end

--- workshop.py
+++ workshop.py
@@ -7,28 +7,20 @@ class Person:
     def introduce(self):
         print(f"Привет, меня зовут {self.name}, мне {self.age} лет, и я живу в {self.location}.")
 
+
 class Teenager(Person):
     def __init__(self, name, age, location, school):
-        self.name = name
-        self.age = age
-        self.location = location
+        super().__init__(name, age, location)
         self.school = school
-
-    def introduce(self):
-        print(f"Привет, меня зовут {self.name}, мне {self.age} лет, и я живу в {self.location}.")
 
     def study(self):
         print(f"Я учусь в {self.school} и учусь очень старательно!")
 
+
 class Adult(Person):
     def __init__(self, name, age, location, job):
-        self.name = name
-        self.age = age
-        self.location = location
+        super().__init__(name, age, location)
         self.job = job
 
-    def introduce(self):
-        print(f"Привет, меня зовут {self.name}, мне {self.age} лет, и я живу в {self.location}.")
-
     def work(self):
-        print(f"Я работаю {self.job} и заботлюсь о своей семье.")
+        print(f"Я работаю {self.job} и стремлюсь стать мидл разработчиком.")
